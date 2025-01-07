8fda2b06c52d8369bb3950e3f5fd67c4e0492955
easbi <easterikhxen@gmail.com>
2025-01-07 10:17:48 +0700

    Perbaikan di admin.index

--- resources/views/home/reservation.blade.php
+++ resources/views/home/reservation.blade.php
@@ -9,8 +9,8 @@
   <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="{{asset('QuickStart/assets/img/favicon.png')}}" rel="icon">
-  <link href="{{asset('QuickStart/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  <link href="{{asset('public/QuickStart/assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('public/QuickStart/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -18,14 +18,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{asset('QuickStart/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{asset('QuickStart/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{asset('QuickStart/assets/vendor/aos/aos.css')}}" rel="stylesheet">
-  <link href="{{asset('QuickStart/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
-  <link href="{{asset('QuickStart/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/QuickStart/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/QuickStart/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('public/QuickStart/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+  <link href="{{asset('public/QuickStart/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('public/QuickStart/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="{{asset('QuickStart/assets/css/main.css')}}" rel="stylesheet">
+  <link href="{{asset('public/QuickStart/assets/css/main.css')}}" rel="stylesheet">
 </head>
 
 <body class="index-page">
@@ -34,7 +34,7 @@
     <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
       <a href="index.html" class="logo d-flex align-items-center me-auto">
-        <img src="{{asset('QuickStart/assets/img/logo.png')}}" alt="">
+        <img src="{{asset('public/QuickStart/assets/img/logo.png')}}" alt="">
         <h1 class="sitename">ITG</h1>
       </a>
 
@@ -230,14 +230,14 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="{{asset('QuickStart/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('QuickStart/assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{asset('QuickStart/assets/vendor/aos/aos.js')}}"></script>
-  <script src="{{asset('QuickStart/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{asset('QuickStart/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('public/QuickStart/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('public/QuickStart/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('public/QuickStart/assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{asset('public/QuickStart/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('public/QuickStart/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
 
   <!-- Main JS File -->
-  <script src="{{asset('QuickStart/assets/js/main.js')}}"></script>
+  <script src="{{asset('public/QuickStart/assets/js/main.js')}}"></script>
 
 </body>
 
